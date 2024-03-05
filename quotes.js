5feb6b7b14197da66b5a42fc7b479920e3140755
JuanmaRamosRodriguez <95094039+JuanmaRamosRodriguez@users.noreply.github.com>
2024-03-05 18:53:39 +0100

    update

---
+++ quotes.js
@@ -0,0 +1,276 @@
+const QUOTEBANK = [{
+          quote: "What you do today can improve all your tomorrows.",
+          author: "Ralph Marston"
+        },
+        {
+          quote: "What you get by achieving your goals is not as important as what you become by achieving your goals.",
+          author: "Zig Ziglar"
+        },
+        {
+          quote: "Intelligence without ambition is a bird without wings.",
+          author: "Salvador Dali"
+        },
+        {
+          quote: "The key is to keep company only with people who uplift you, whose presence calls forth your best.",
+          author: "Epictetus"
+        },
+        {
+          quote: "We may encounter many defeats but we must not be defeated.",
+          author: "May Angelou"
+        },
+        {
+          quote: "Problems are not stop signs, they are guidelines.",
+          author: "Robert H. Schuller"
+        },
+        {
+          quote: "Be kind whenever possible. It is always possible.",
+          author: "Dalai Lama"
+        },
+        {
+          quote: "A creative man is motivated by the desire to achieve, not by the desire to beat others.",
+          author: "Ayn Rand"
+        },
+        {
+          quote: "A somebody was once a nobody who wanted to and did.",
+          author: "John Burroughs"
+        },
+        {
+          quote: "Life is 10% what happens to us and 90% how we react to it.",
+          author: "Dennis P. Kimbro"
+        },
+        {
+          quote: "Even if you’re on the right track, you’ll get run over if you just sit there.",
+          author: "Will Rogers"
+        },
+        {
+          quote: "Luck is a dividend of sweat. The more you sweat, the luckier you get.",
+          author: "Ray Kroc"
+        },
+        {
+          quote: "Courage is the first of human qualities because it is the quality which guarantees all others.",
+          author: "Winston Churchill"
+        },
+        {
+          quote: "The great thing in this world is not so much where you stand, as in what direction you are moving.",
+          author: "Oliver Wendell Holmes"
+        },
+        {
+          quote: "Though no one can go back and make a brand new start, anyone can start from now and make a brand new ending.",
+          author: "Carl Bard"
+        },
+        {
+          quote: "Speak less than you know; have more than you show.",
+          author: "William Shakespeare"
+        },
+        {
+          quote: "Education costs money. But then so does ignorance.",
+          author: "Sir Claus Moser"
+        },
+        {
+          quote: "The journey of a thousand miles begins with one step.",
+          author: "Lao Tzu"
+        },
+        {
+          quote: "Do not let what you cannot do interfere with what you can do.",
+          author: "John Wooden"
+        },
+        {
+          quote: "Defeat is not bitter unless you swallow it.",
+          author: "Joe Clark"
+        },
+        {
+          quote: "An obstacle is often a stepping stone.",
+          author: "Prescott"
+        },
+        {
+          quote: "Don’t worry about failures, worry about the chances you miss when you don’t even try.",
+          author: "Jack Canfield"
+        },
+        {
+          quote: "Live each day as if your life had just begun.",
+          author: "Johann Wolfgang Von Goethe"
+        },
+        {
+          quote: "The secret to getting ahead is getting started.",
+          author: "Mark Twain"
+        },
+        {
+          quote: "The will to win, the desire to succeed, the urge to reach your full potential, these are the keys that will unlock the door to personal excellence.",
+          author: "Confucius"
+        },
+        {
+          quote: "Set your goals high and don’t stop until you get there.",
+          author: "Bo Jackson"
+        },
+        {
+          quote: "The world is full of magical things patiently waiting for our wits to grow sharper.",
+          author: "Bertrand Russell"
+        },
+        {
+          quote: "Let us make our future now, and let us make our dreams tomorrow’s reality.",
+          author: "Malala Yousafzai"
+        },
+        {
+          quote: "All you need is the plan, the road map, and the courage to press on to your destination.",
+          author: "Earl Nightingale"
+        },
+        {
+          quote: "The glow of one warm thought is to me worth more than money.",
+          author: "Thomas Jefferson"
+        },
+        {
+          quote: "The power of imagination makes us infinite.",
+          author: "John Muir"
+        },
+        {
+          quote: "Try to be a rainbow in someone’s cloud.",
+          author: "Maya Angelou"
+        },
+        {
+          quote: "I dwell in possibility.",
+          author: "Emily Dickinson"
+        },
+        {
+          quote: "A No. 2 pencil and a dream can take you anywhere.",
+          author: "Joyce Meyer"
+        },
+        {
+          quote: "In a gentle way, you can shake the world.",
+          author: "Mahatma Gandhi"
+        },
+        {
+          quote: "Follow your bliss and the universe will open doors where there were only walls.",
+          author: "Joseph Campbell"
+        },
+        {
+          quote: "Each day provides its own gifts.",
+          author: "Marcus Aurelius"
+        },
+        {
+          quote: "Creativity is intelligence having fun.",
+          author: "Albert Einstein"
+        },
+        {
+          quote: "People who are crazy enough to think they can change the world, are the ones who do.",
+          author: "Rob Siltanen"
+        },
+        {
+          quote: "Imagine your life is perfect in every respect; what would it look like?",
+          author: "Brian Tracy"
+        },
+        {
+          quote: "The way to get started is to quit talking and begin doing.",
+          author: "Walt Disney"
+        },
+        {
+          quote: "It is always the simple that produces the marvelous.",
+          author: "Amelia Barr"
+        },
+        {
+          quote: "Our greatest glory is not in never falling, but in rising every time we fall.",
+          author: "Confucius"
+        },
+        {
+          quote: "Everything you’ve ever wanted is on the other side of fear.",
+          author: "George Addair"
+        },
+        {
+          quote: "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+          author: "Winston Churchill"
+        },
+        {
+          quote: "Hardships often prepare ordinary people for an extraordinary destiny.",
+          author: "C.S. Lewis"
+        },
+        {
+          quote: "Believe in yourself. You are braver than you think, more talented than you know, and capable of more than you imagine.",
+          author: "Roy T. Bennett"
+        },
+        {
+          quote: "I learned that courage was not the absence of fear, but the triumph over it. The brave man is not he who does not feel afraid, but he who conquers that fear.",
+          author: "Nelson Mandela"
+        },
+        {
+          quote: "There is only one thing that makes a dream impossible to achieve: the fear of failure.",
+          author: "Paulo Coelho"
+        },
+        {
+          quote: "It’s not whether you get knocked down. It’s whether you get up.",
+          author: "Vince Lombardi"
+        },
+        {
+          quote: "Your true success in life begins only when you make the commitment to become excellent at what you do.",
+          author: "Brian Tracy"
+        },
+        {
+          quote: "If you believe it will work out, you’ll see opportunities. If you believe it won’t, you will see obstacles.",
+          author: "Wayne Dyer"
+        },
+        {
+          quote: "Your mental and physical health come first. Everything else is secondary.",
+          author: "Zat Rana"
+        },
+        {
+          quote: "Less is almost always more. Simplicity is almost always the answer.",
+          author: "Zat Rana"
+        },
+        {
+          quote: "You have to be lucky to be successful, but luck can be engineered.",
+          author: "Zat Rana"
+        },
+        {
+          quote: "It all starts and ends in the mind. The most crucial skill is how you think.",
+          author: "Zat Rana"
+        },
+        {
+          quote: "People suck. But if you’re kind, you’ll get the best out of even the worst.",
+          author: "Zat Rana"
+        },
+        {
+          quote: "Perfection only exists in the mind. It’s not real. Imagine, create, improve.",
+          author: "Zat Rana"
+        },
+        {
+          quote: "Jamás pensé que moriría peleando junto a un elfo.",
+          author: "Zat Rana"
+        },
+        {
+          quote: "Struggle is a part of life. Learning to struggle well is a reward in itself.",
+          author: "Gimli"
+        },
+        {
+          quote: "Jamás pensé que moriría peleando junto a un elfo.",
+          author: "Gandalf el Blanco"
+        },
+        {
+          quote: "Los Hobbits son criaturas sorprendentes, puedes aprender todas sus costumbres en un mes, y después de cien años, aún te sorprenden.",
+          author: "Gandalf el Gris"
+        },
+        {
+          quote: "En medio de la dificultad reside la oportunidad",
+          author: "Albert Einstein"
+        },
+        {
+          quote: "El valor de una idea radica en el uso de la misma",
+          author: "Tomas A.Edison"
+        },
+        {
+          quote: "La música es el arte más directo, entra por el oído y va al corazón.",
+          author: "Magdalena Martínez"
+        },
+        {
+          quote: "La pobreza no viene por la disminución de las riquezas, sino por la multiplicación de los deseos.",
+          author: "Platón"
+        },
+        {
+          quote:"Un hombre que alaba a un hombre en su cara está lleno de motivos ocultos",
+          author:"Senku Ichigami"
+        },
+        {
+          quote:"A veces hay que dejar de lado el orgullo y hacer lo que se nos pide.",
+          author:"Anakin Skywalker"
+        }
+          
+        
+
+]
